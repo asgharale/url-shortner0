89a19f55f235cecd9008bdac8a1ada2ecc37cef7
asghar ale <asgharale11@gmail.com>
2026-07-11 00:12:25 +0330

    fix backend

--- backend/shortener/urls.py
+++ backend/shortener/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path("shorten/", views.shorten_url, name="shorten-url"),
     path("pro/shorten/", views.shorten_pro_url, name="shorten-pro-url"),
     path("wheel/redirect/", views.latest_wheel_redirect, name="latest-wheel-redirect"),
+    path("<str:short_code>", views.redirect_short_code, name="redirect-short-code"),
 ]
